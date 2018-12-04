f206f12275bb0454405ea36b45ddfdca6eaa1099
Shlomi Vaknin <shlomivaknin@gmail.com>
2018-12-04 13:36:59 -0800

    Added proper support for grouping

--- papiea-lib-clj/test/papiea-lib-clj/core_test.cljs
+++ papiea-lib-clj/test/papiea-lib-clj/core_test.cljs
@@ -3,6 +3,10 @@
             [instaparse.core :as insta]
             [papiea-lib-clj.core :as c]))
 
+(defn unordered= [& xs]
+  (and (apply = (map count xs))
+   (apply = (map set xs))))
+
 (deftest ambigous-parser
   ;; An example of the original, ambigous parser
   (let [sfs-parser-ambigous (insta/parser
@@ -18,7 +22,7 @@
                               <field> = #'[a-zA-Z_0-9]+'    
                               ")]
     (testing "ambigous parser returns multiple results"
-      (is (= (insta/parses sfs-parser-ambigous "a.v.[+{a}.a,d]")
+      (is (unordered= (insta/parses sfs-parser-ambigous "a.v.[+{a}.a,d]")
              [[:S
                [:complex
                 [:simple "a"]
@@ -35,7 +39,7 @@
 
 (deftest unambiguous-parser
   (testing "unambiguous parser returns a single result"
-    (is (= (insta/parses c/sfs-parser "a.v.[+{a}.a,d]")
+    (is (unordered= (insta/parses c/sfs-parser "a.v.[+{a}.a,d]")
            [[:S
              [:complex
               [:simple "a" "v"]
@@ -44,7 +48,7 @@
                [:simple "d"]]]]])))
   
   (testing "Ability to have a mix of simple, complex and grouping of any length, without introducing ambiguity"
-    (is(= (insta/parses c/sfs-parser "a.1.2.{b.1.2}.c.1.2.[d.1.2].e.1.2.{d.1.2}")
+    (is(unordered= (insta/parses c/sfs-parser "a.1.2.{b.1.2}.c.1.2.[d.1.2].e.1.2.{d.1.2}")
           [[:S
              [:complex
               [:simple "a" "1" "2"]
@@ -55,7 +59,7 @@
               [:vector [:change] "d" "1" "2"]]]])))
 
   (testing "Check vector add, del, change and group"
-    (is (= (insta/parse c/sfs-parser "a.-{s.c}.[+{d},f.{d}.f.d]")
+    (is (unordered= (insta/parse c/sfs-parser "a.-{s.c}.[+{d},f.{d}.f.d]")
            [:S
             [:complex
              [:simple "a"]
@@ -82,7 +86,7 @@
 
 (deftest prepare
   (testing "creating an initial context from a spec/status pair"
-    (is (= (c/prepare
+    (is (unordered= (c/prepare
             ;; Spec portion
             {:f1 [{:id 2 :props [{:id2 4 :name "n1"}]}
                   {:id 1 :props [{:id2 5 :name "n2"}]}]}
@@ -104,14 +108,14 @@
 
 (deftest compiler
   (testing "simple"
-    (is (= ((c/sfs-compiler[:papiea/simple :name])
+    (is (unordered= ((c/sfs-compiler[:papiea/simple :name])
             [{:keys {} :key :papiea/item
               :spec-val [{:name "old"}]
               :status-val [{:name "new"}]}])
            
            [{:keys {}, :key :name, :spec-val ["old"], :status-val ["new"]}]))
 
-    (is (= ((c/sfs-compiler[:papiea/simple :v])
+    (is (unordered= ((c/sfs-compiler[:papiea/simple :v])
             [{:keys {} :key :papiea/item
               :spec-val [{:v [{:name "old"}
                               {:name "a"}]}]
@@ -124,7 +128,7 @@
              :status-val [{:name "new"} {:name "n"}]}]))
 
     (testing "given a previous context"
-      (is (= ((c/sfs-compiler [:papiea/simple :name])
+      (is (unordered= ((c/sfs-compiler [:papiea/simple :name])
               [{:keys {:id 1} :key :papiea/item :spec-val [{:id 1 :name "old1"}] :status-val [{:id 1 :name "new1"}]}
                {:keys {:id 2} :key :papiea/item :spec-val [{:id 2 :name "old2"}] :status-val [{:id 2 :name "new2"}]}
                {:keys {:id 3} :key :papiea/item :spec-val [] :status-val [{:id 3 :name "old3"}]}])
@@ -135,7 +139,7 @@
   
   (testing "vector"
     (testing "all changes"
-      (is (= ((c/sfs-compiler[:papiea/vector [:all] :id])
+      (is (unordered= ((c/sfs-compiler[:papiea/vector [:all] :id])
               [{:keys       {} :key :papiea/item
                 :spec-val   [{:id 1 :name "old1"} {:id 2 :name "old2"}]
                 :status-val [{:id 2 :name "new2"} {:id 1 :name "new1"} {:id 3 :name "old3"}]}])
@@ -145,7 +149,7 @@
               {:keys {:id 3} :key :papiea/item :spec-val [] :status-val [{:id 3 :name "old3"}]}])))
 
     (testing "`add` changes"
-      (is (= ((c/sfs-compiler[:papiea/vector [:add] :id])
+      (is (unordered= ((c/sfs-compiler[:papiea/vector [:add] :id])
               [{:keys       {} :key :papiea/item
                 :spec-val   [{:id 1 :name "old1"} {:id 2 :name "old2"} {:id 4 :name "new4"}]
                 :status-val [{:id 2 :name "new2"} {:id 1 :name "new1"} {:id 3 :name "old3"}]}])
@@ -156,7 +160,7 @@
                :status-val []}])))
 
     (testing "`del` changes"
-      (is (= ((c/sfs-compiler[:papiea/vector [:del] :id])
+      (is (unordered= ((c/sfs-compiler[:papiea/vector [:del] :id])
               [{:keys       {} :key :papiea/item
                 :spec-val   [{:id 1 :name "old1"} {:id 2 :name "old2"}]
                 :status-val [{:id 2 :name "new2"} {:id 1 :name "new1"} {:id 3 :name "old3"}]}])
@@ -168,7 +172,7 @@
              ))))
 
   (testing "group"
-    (is (= ((c/sfs-compiler [:papiea/group [:papiea/simple :name] [:papiea/simple :age]])
+    (is (unordered= ((c/sfs-compiler [:papiea/group [:papiea/simple :name] [:papiea/simple :age]])
             (c/prepare
              {:name "a" :age 23}
              {:name "b" :age 12}))
@@ -177,7 +181,7 @@
             {:keys {} :key :age :spec-val [23] :status-val [12]}])))
 
   (testing "complex"
-    (is (= ((c/sfs-compiler [:papiea/complex [:papiea/simple :f1] [:papiea/vector [:all] :id] [:papiea/simple :props] [:papiea/vector [:all] :id2] [:papiea/simple :name]])
+    (is (unordered= ((c/sfs-compiler [:papiea/complex [:papiea/simple :f1] [:papiea/vector [:all] :id] [:papiea/simple :props] [:papiea/vector [:all] :id2] [:papiea/simple :name]])
             (c/prepare
              {:f1 [{:id 2 :props [{:id2 4 :name "n1"}]}
                    {:id 1 :props [{:id2 5 :name "n2"}]}]}
@@ -188,7 +192,7 @@
             {:keys {:id 1, :id2 5}, :key :name, :spec-val ["n2"], :status-val ["o2"]}]))
 
 
-    (is (= ((c/sfs-compiler [:papiea/complex
+    (is (unordered= ((c/sfs-compiler [:papiea/complex
                              [:papiea/simple :f1]
                              [:papiea/vector [:all] :id]
                              [:papiea/group
@@ -203,15 +207,15 @@
              {:f1 [{:id 1 :another "a1_old" :props [{:id2 5 :name "o2"}]}
                    {:id 2 :another "a2_old" :props [{:id2 4 :name "o1"}]}]}))
 
-           [{:keys {:id 2}, :key :another, :spec-val ["a2"], :status-val ["a2_old"]}
-            {:keys {:id 1}, :key :another, :spec-val ["a1"], :status-val ["a1_old"]}
-            {:keys {:id 2, :id2 4}, :key :name, :spec-val ["n1"], :status-val ["o1"]}
-            {:keys {:id 1, :id2 5}, :key :name, :spec-val ["n2"], :status-val ["o2"]}]))))
+           [{:keys {:id 2},:key :another,:spec-val ["a2"],:status-val ["a2_old"]}
+            {:keys {:id 2, :id2 4},:key :name,:spec-val ["n1"],:status-val ["o1"]}
+            {:keys {:id 1},:key :another,:spec-val ["a1"],:status-val ["a1_old"]}
+            {:keys {:id 1, :id2 5},:key :name,:spec-val ["n2"],:status-val ["o2"]}]))))
 
 (deftest optimizer
   (testing "optimizer adds namepsaces to the AST node keywords"
     (testing "not optimized"
-      (is (= (insta/parse c/sfs-parser "f1.{id}.[another,props.{id2}.name]")
+      (is (unordered= (insta/parse c/sfs-parser "f1.{id}.[another,props.{id2}.name]")
 
              [:S
               [:complex
@@ -226,7 +230,7 @@
 
     (testing "optimized"
       (is 
-       (= (c/optimize-ast (insta/parse c/sfs-parser "f1.{id}.[another,props.{id2}.name]"))
+       (unordered= (c/optimize-ast (insta/parse c/sfs-parser "f1.{id}.[another,props.{id2}.name]"))
 
           [:S
            [:papiea/complex
@@ -242,7 +246,7 @@
   (testing "optimizer removes singelton `complex` nodes,"
     (testing "not optimized"
       (is 
-       (= (insta/parse c/sfs-parser "[{a.v},d]")
+       (unordered= (insta/parse c/sfs-parser "[{a.v},d]")
 
           [:S
            [:complex   ;; Singleton complex, can be eliminated
@@ -252,7 +256,7 @@
     
     (testing "optimized"
       (is 
-       (= (c/optimize-ast (insta/parse c/sfs-parser "[{a.v},d]"))
+       (unordered= (c/optimize-ast (insta/parse c/sfs-parser "[{a.v},d]"))
           [:S
            [:papiea/group
             [:papiea/vector [:change] "a" "v"]
@@ -262,7 +266,7 @@
   (testing "Multiple features: parsing, optimizing, compiling and running with grouping where one branch has another vector. See the `:keys` attribute in the results."
 
     (testing "Test using clojure native structures using keywords")
-    (is (= ((c/sfs-compiler (c/optimize-ast (insta/parse c/sfs-parser "f1.{id}.[another, props.{id2}.name]")))
+    (is (unordered= ((c/sfs-compiler (c/optimize-ast (insta/parse c/sfs-parser "f1.{id}.[another, props.{id2}.name]")))
             (c/prepare
              {:f1 [{:id 2 :another "a2" :props [{:id2 4 :name "n1"}]}
                    {:id 1 :another "a1" :props [{:id2 5 :name "n2"}]}]}
@@ -275,25 +279,30 @@
             {:keys {"id" 1, "id2" 5},:key "name",:spec-val ["n2"],:status-val ["o2"]}]))
 
     (testing "Test using javascript-like stracture using strings"
-      (is (= ((c/sfs-compiler (c/optimize-ast (insta/parse c/sfs-parser "f1.{id}.[another, props.{id2}.name]")))
+      (is (unordered= ((c/sfs-compiler (c/optimize-ast (insta/parse c/sfs-parser "f1.{id}.[another, props.{id2}.name]")))
               (c/prepare
                {"f1" [{"id" 2 "another" "a2" "props" [{"id2" 4 "name" "n1"}]}
                       {"id" 1 "another" "a1" "props" [{"id2" 5 "name" "n2"}]}]}
                {"f1" [{"id" 1 "another" "a1_old" "props" [{"id2" 5 "name" "o2"}]}
                       {"id" 2 "another" "a2_old" "props" [{"id2" 4 "name" "o1"}]}]}))
 
+             
+             
            [{:keys {"id" 2},:key "another",:spec-val ["a2"],:status-val ["a2_old"]}
             {:keys {"id" 1},:key "another",:spec-val ["a1"],:status-val ["a1_old"]}
             {:keys {"id" 2, "id2" 4},:key "name",:spec-val ["n1"],:status-val ["o1"]}
             {:keys {"id" 1, "id2" 5},:key "name",:spec-val ["n2"],:status-val ["o2"]}])))
 
-    (testing "Even a single diff missing should results in nil"
-      (is (nil? ((c/sfs-compiler (c/optimize-ast (insta/parse c/sfs-parser "f1.{id}.[another, props.{id2}.name]")))
-              (c/prepare
-               {"f1" [{"id" 2 "another" "a2" "props" [{"id2" 4 "name" "n1"}]}
-                      {"id" 1 "another" "a1" "props" [{"id2" 5 "name" "n2"}]}]}
-               {"f1" [{"id" 1 "another" "a1_old" "props" [{"id2" 5 "name" "o2"}]}
-                      {"id" 2 "another" "a2" "props" [{"id2" 4 "name" "o1"}]}]})))))))
+    (testing "Items with an incomplete diff should be fully removed from the results"
+      (is (unordered= ((c/sfs-compiler (c/optimize-ast (insta/parse c/sfs-parser "f1.{id}.[another, props.{id2}.name]")))
+                       (c/prepare
+                        {"f1" [{"id" 2 "another" "a2" "props" [{"id2" 4 "name" "n1"}]}
+                               {"id" 1 "another" "a1" "props" [{"id2" 5 "name" "n2"}]}]}
+                        {"f1" [{"id" 1 "another" "a1_old" "props" [{"id2" 5 "name" "o2"}]}
+                               {"id" 2 "another" "a2" "props" [{"id2" 4 "name" "o1"}]}]}))
+
+                      [{:keys {"id" 1},:key "another",:spec-val ["a1"],:status-val ["a1_old"]}
+                       {:keys {"id" 1, "id2" 5},:key "name",:spec-val ["n2"],:status-val ["o2"]}])))))
 
 (deftest ensure-diff-exists
   (testing "all potential diffs has to really resolve to different values."
@@ -308,9 +317,74 @@
                             {:a 2 :v 2})))))
 
     (testing "all different"
-      (is (= ((c/sfs-compiler (c/optimize-ast (insta/parse c/sfs-parser "[a,v]")))
+      (is (unordered= ((c/sfs-compiler (c/optimize-ast (insta/parse c/sfs-parser "[a,v]")))
               (c/prepare {:a 1 :v 2}
                          {:a 2 :v 4}))
 
              [{:keys {}, :key "a", :spec-val [1], :status-val [2]}
               {:keys {}, :key "v", :spec-val [2], :status-val [4]}])))))
+
+
+(deftest grouping-tests
+  (testing "Must have at least one of each branch"
+    (is (unordered= ((c/sfs-compiler (c/optimize-ast (insta/parse c/sfs-parser "f1.{id}.[drive_list.{letter}.name, network_list.{mac}.ip]")))
+                     (c/prepare
+                      {"f1" [{"id" 2 "drive_list" [{"letter" "a" "name" "a"}
+                                                   {"letter" "b" "name" "b"}] "network_list" [{"mac" 4 "ip" "ip2"}
+                                                                                              {"mac" 5 "ip" "ip1"}]}]}
+                      {"f1" [{"id" 2 "drive_list" [{"letter" "a" "name" "a"}
+                                                   {"letter" "b" "name" "a"}] "network_list" [{"mac" 4 "ip" "ip1"}
+                                                                                              {"mac" 5 "ip" "ip1"}]}]}))
+
+                    [{:keys {"id" 2, "letter" "b"},:key "name",:spec-val ["b"],:status-val ["a"]}
+                     {:keys {"id" 2, "mac" 4},:key "ip",:spec-val ["ip2"],:status-val ["ip1"]}])))
+
+
+  (testing "Will return all items that match"
+    (is (unordered= ((c/sfs-compiler (c/optimize-ast (insta/parse c/sfs-parser "f1.{id}.[name, network_list.{mac}.ip]")))
+                     (c/prepare
+                      {"f1" [{"id" 2 "name" "a2" "network_list" [{"mac" 4 "ip" "ip1"}
+                                                                 {"mac" 5 "ip" "ip1"}
+                                                                 {"mac" 6 "ip" "ip1"}]}]}
+                      {"f1" [{"id" 2 "name" "a1" "network_list" [{"mac" 4 "ip" "ip2"}
+                                                                 {"mac" 5 "ip" "ip2"}
+                                                                 {"mac" 6 "ip" "ip1"}]}]}))
+
+                    [{:keys {"id" 2}, :key "name", :spec-val ["a2"], :status-val ["a1"]}
+                     {:keys {"id" 2, "mac" 4},:key "ip",:spec-val ["ip1"],:status-val ["ip2"]}
+                     {:keys {"id" 2, "mac" 5},:key "ip",:spec-val ["ip1"],:status-val ["ip2"]}])))
+
+  (testing "Match on an entire vector item"
+    (is (unordered= ((c/sfs-compiler (c/optimize-ast (insta/parse c/sfs-parser "f1.{id}.[name, network_list]")))
+                     (c/prepare
+                      {"f1" [{"id" 2 "name" "a2" "network_list" [{"mac" 4 "ip" "ip1"}
+                                                                 {"mac" 5 "ip" "ip1"}]}]}
+                      {"f1" [{"id" 2 "name" "a1" "network_list" [{"mac" 4 "ip" "ip2"}
+                                                                 {"mac" 5 "ip" "ip2"}]}]}))
+
+                    [{:keys {"id" 2}, :key "name", :spec-val ["a2"], :status-val ["a1"]}
+                     {:keys {"id" 2},
+                      :key "network_list",
+                      :spec-val [{"mac" 4, "ip" "ip1"} {"mac" 5, "ip" "ip1"}],
+                      :status-val [{"mac" 4, "ip" "ip2"} {"mac" 5, "ip" "ip2"}]}])))
+
+  (testing "Using simple query, ensure all branches match"
+    (is (nil? ((c/sfs-compiler (c/optimize-ast (insta/parse c/sfs-parser "f1.{id}.[another, name]")))
+               (c/prepare
+                {"f1" [{"id" 2 "another" "a1" "name" "binny"}
+                       {"id" 1 "another" "a1" "name" "shlomi"}]}
+                {"f1" [{"id" 1 "another" "a1" "name" "shlomi"}
+                       {"id" 2 "another" "a2" "name" "binny"}]}))))
+
+    (is (unordered= ((c/sfs-compiler (c/optimize-ast (insta/parse c/sfs-parser "f1.{id}.[another, name]")))
+                     (c/prepare
+                      {"f1" [{"id" 2 "another" "a1" "name" "shlomi"}
+                             {"id" 1 "another" "a1" "name" "shlomi"}]}
+                      {"f1" [{"id" 1 "another" "a1" "name" "shlomi"}
+                             {"id" 2 "another" "a2" "name" "binny"}]}))
+
+                    [{:keys {"id" 2},:key "another",:spec-val ["a1"],:status-val ["a2"]}
+                     {:keys {"id" 2},:key "name",:spec-val ["shlomi"],:status-val ["binny"]}]))))
+
+
+
--- src/main.ts
+++ src/main.ts
@@ -3,33 +3,51 @@ var app = express();
 
 var papi_clj = require("../papiea-lib-clj/papiea-lib-clj.js").papiea_lib_clj;
 
-export const Greeter = (name: string) => `Hello!! ${name}`; 
-
-console.log(Greeter(papi_clj.core.foo("aaabaabbb", 3)));
-
+//export const Greeter = (name: string) => `Hello!! ${name}`; 
 
 export const clj_str = (a:any) => papi_clj.core.clj_str(a);
 export const sfs_compile = (sfs_signature: string) => papi_clj.core.compile_sfs(sfs_signature);
 export const sfs_parser = (sfs_ast: string) => papi_clj.core.parse_sfs(sfs_ast);
 export const sfs_optimizer = (sfs_ast: string) => papi_clj.core.optimize_sfs_ast(sfs_ast);
 export const sfs_compiler = (sfs_signature: string) => papi_clj.core.compile_sfs(sfs_signature);
-export const run_compiled_sfs = (compiled_sfs:any, spec:any, status:any) => papi_clj.core.run_compiled_sfs(compiled_sfs, spec, status);
+export const run_compiled_sfs = (compiled_sfs:any, spec:any, status:any) =>
+    papi_clj.core.run_compiled_sfs(compiled_sfs, spec, status);
 
 console.log("print simple sfs ast for: 'a'")
 console.log(clj_str(sfs_parser("a")))
 console.log("")
+
 console.log("print complex sfs ast for: 'f1.{id}.[another, props.{id2}.name]'")
 console.log(clj_str(sfs_parser("f1.{id}.[another, props.{id2}.name]")))
-
 console.log("")
+
 console.log("print non-optimized sfs ast for: '[{a.v},d]'")
 console.log(clj_str(sfs_parser("[{a.v},d]")))
-
 console.log("")
+
 console.log("print optimized sfs ast for: '[{a.v},d]'") // simpler case {a.v}
 console.log(clj_str(sfs_optimizer(sfs_parser("[{a.v},d]"))))
-
 console.log("")
+
+{
+    console.log("Running the sfs '[a,d]' on spec:{a:1, d:2} status:{a:3, d:4}") // simpler case {a.v}
+    let sfs = sfs_compiler("[a,d]")
+    let r = run_compiled_sfs(sfs, {"a":1, "d":2}, {"a":3, "d":4})
+    console.log("results", r)
+    console.log("")
+}
+
+{
+    console.log("Running the sfs 'a.{id}.[a,d]' on spec:{a:1, d:2} status:{a:3, d:4}") // simpler case {a.v}
+    let sfs = sfs_compiler("a.{id}.[a,d]")
+    let r = run_compiled_sfs(sfs,
+                             {"a":[{"id": 1, "a":1, "d":2},
+                                   {"id": 2, "a":1, "d":2}]},
+                             {"a":[{"id": 1, "a":2, "d":3},
+                                   {"id": 2, "a":1, "d":3}]})
+    console.log("results", r)
+    console.log("")
+}
 
 app.get('/', function (req:any, res:any) {
   res.send('Hello World!');

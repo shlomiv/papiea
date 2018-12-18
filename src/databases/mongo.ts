84f859851f2418ff3916002c0ca880d7071642f1
Aleksandr Dolgaryev <adolgarev@gmail.com>
2018-12-18 12:30:41 +0200

    DAL for status

--- src/databases/mongo.ts
+++ src/databases/mongo.ts
@@ -1,5 +1,6 @@
 import {MongoClient,Db} from "mongodb";
 import {Spec_DB_Mongo} from "./spec_db_mongo";
+import {Status_DB_Mongo} from "./status_db_mongo";
 
 export class MongoConnection {
     url:string;
@@ -7,12 +8,14 @@ export class MongoConnection {
     client:MongoClient;
     db:Db|undefined;
     specDb:Spec_DB_Mongo|undefined;
+    statusDb:Status_DB_Mongo|undefined;
     constructor(url:string, dbName:string) {
         this.url = url;
         this.dbName = dbName;
         this.client = new MongoClient(this.url, { useNewUrlParser: true });
         this.db = undefined;
         this.specDb = undefined;
+        this.statusDb = undefined;
     }
 
     connect(cb: (error?:Error) => void):void {
@@ -38,6 +41,19 @@ export class MongoConnection {
             if (err)
                 return cb(err);
             cb(null, this.specDb);
+        });
+    }
+
+    get_status_db(cb: (error: Error|null, statusDb?: Status_DB_Mongo) => void):void {
+        if (this.statusDb !== undefined)
+            return cb(null, this.statusDb);
+        if (this.db === undefined)
+            return cb(new Error("Not connected"));
+        this.statusDb = new Status_DB_Mongo(this.db);
+        this.statusDb.init(err => {
+            if (err)
+                return cb(err);
+            cb(null, this.statusDb);
         });
     }
 }

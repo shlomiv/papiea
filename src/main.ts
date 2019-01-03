bd6379522a38ae2e0b82fea9513d8a498d6566d1
Aleksandr Dolgaryev <adolgarev@gmail.com>
2019-01-03 17:32:55 +0200

    Swagger spec for entity CRUD - get request

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import * as express from "express";
+import createAPIDocsRouter from "./api_docs";
 import createProviderAPIRouter from "./provider/provider_routes";
 import { Provider_API_Impl } from "./provider/provider_api_impl";
 import { MongoConnection } from "./databases/mongo";
-const swaggerUi = require('express-swaggerize-ui');
 
 declare var process: {
     env: {
@@ -18,16 +18,12 @@ const serverPort = parseInt(process.env.SERVER_PORT || '3000');
 async function setUpApplication(): Promise<express.Express> {
     const app = express();
     app.use(express.json());
-    app.use('/api-docs.json', function (req, res) {
-        // TODO(adolgarev): generate swagger on the fly
-        // res.json(...);
-    });
-    app.use('/api-docs', swaggerUi({ route: '/api-docs', docs: 'https://petstore.swagger.io/v2/swagger.json' }));
     const mongoConnection: MongoConnection = new MongoConnection(process.env.MONGO_URL || 'mongodb://mongo:27017', process.env.MONGO_DB || 'papiea');
     await mongoConnection.connect();
     const providerDb = await mongoConnection.get_provider_db();
     const statusDb = await mongoConnection.get_status_db();
     app.use('/provider', createProviderAPIRouter(new Provider_API_Impl(providerDb, statusDb)));
+    app.use('/api-docs', createAPIDocsRouter('/api-docs', providerDb));
     app.use(function (err: any, req: any, res: any, next: any) {
         if (res.headersSent) {
             return next(err);

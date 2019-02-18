fadee45365dcb8bd7a18d0f9e9de081c408fa4ef
Danylo Lytovchenko <evlen@qip.ru>
2019-02-18 10:00:31 +0200

    add script to login into dockerhub

---
+++ docker-login.py
@@ -0,0 +1,9 @@
+import subprocess
+import os
+
+
+subprocess.check_call([
+    'docker', 'login',
+    'nutanix-docker.jfrog.io',
+    '-u', os.environ['ARTIFACTORY_USER'],
+    '-p', os.environ['ARTIFACTORY_PASSWORD']])

918cc2ea797d6531773ed8f7262276de6a7d4b7e
Danylo Lytovchenko <evlen@qip.ru>
2019-04-22 12:39:35 +0300

    Add dockerfile, docker-compose and start script for dev

---
+++ start-dev.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ -d "/node_modules" ] && [ ! -d "node_modules" ]; then
+    echo "Use cached node_modules"
+    mv /node_modules node_modules
+fi
+
+export DEBUG=express:* 
+npm install
+npm run build-clj
+npm run dev

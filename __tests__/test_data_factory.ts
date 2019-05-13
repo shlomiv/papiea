e7de153f7b9214f8f3e25db1a92a100298f22fee
igorkorotach <igorkorotach@gmail.com>
2019-05-13 13:18:38 +0300

    added provider builder

--- __tests__/test_data_factory.ts
+++ __tests__/test_data_factory.ts
@@ -2,14 +2,14 @@ import { load } from "js-yaml";
 import { readFileSync } from "fs";
 import { resolve } from "path";
 import { plural } from "pluralize";
-import { Provider, SpecOnlyEntityKind, Procedural_Signature, Procedural_Execution_Strategy } from "../src/papiea";
-import { Data_Description, Provider_Callback_URL } from "../src/core";
+import { Provider, SpecOnlyEntityKind, Procedural_Signature, Procedural_Execution_Strategy, Kind } from "../src/papiea";
+import { Data_Description, Provider_Callback_URL, Version } from "../src/core";
 
 function randomString(len: number) {
-    var charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var randomString = '';
-    for (var i = 0; i < len; i++) {
-        var randomPoz = Math.floor(Math.random() * charSet.length);
+    const charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+    for (let i = 0; i < len; i++) {
+        let randomPoz = Math.floor(Math.random() * charSet.length);
         randomString += charSet.substring(randomPoz, randomPoz + 1);
     }
     return randomString;
@@ -48,6 +48,161 @@ export function getSpecOnlyEntityKind(): SpecOnlyEntityKind {
         differ: undefined,
     };
     return spec_only_kind;
+}
+
+function formatErrorMsg(current_field: string, missing_field: string) {
+    return `Please specify ${missing_field} before ${current_field}`
+}
+
+export class ProviderBuilder {
+    private readonly _prefix: string;
+    private _version: Version = "0.1.0";
+    private _kinds: Kind[] = [];
+    private _procedures: { [key: string]: Procedural_Signature; } = {};
+    private _oauth2: any;
+    private _extension_structure: any;
+    private _policy: any;
+
+    constructor(prefix?: string) {
+        if (prefix !== undefined) {
+            this._prefix = prefix;
+        } else {
+            this._prefix = randomString(12);
+        }
+        return this;
+    }
+
+    public build(): Provider {
+        const provider: Provider = { prefix: this._prefix, version: this._version, kinds: this._kinds,
+            oauth2: this._oauth2, procedures: this._procedures, extension_structure: this._extension_structure, policy: this._policy};
+        return provider;
+    }
+
+    get prefix(): string {
+        return this._prefix;
+    }
+
+    get oauth2(): any {
+        return this._oauth2;
+    }
+
+    get policy(): any {
+        return this._policy
+    }
+
+    public withPolicy(value?: any) {
+        if (value === undefined) {
+            this._policy = undefined
+        } else {
+            this._policy = value
+        }
+        return this;
+    }
+
+    get extensionStructure() {
+        return this._extension_structure;
+    }
+
+    public withExtensionStructure(value?: any) {
+        if (value === undefined) {
+            this._extension_structure = undefined
+        } else {
+            this._extension_structure = value
+        }
+        return this;
+    }
+
+    public withOAuth2Description(value?: any) {
+        if (value === undefined) {
+            this._oauth2 = loadYaml("./auth.yaml");
+        } else {
+            this._oauth2 = value
+        }
+        return this;
+    }
+
+    public withProviderProcedures(callback: string, value?: { [p: string]: Procedural_Signature }) {
+        if (value === undefined) {
+            const proceduralSignatureForProvider: Procedural_Signature = {
+                name: "computeSum",
+                argument: loadYaml("./procedure_sum_input.yml"),
+                result: loadYaml("./procedure_sum_output.yml"),
+                execution_strategy: Procedural_Execution_Strategy.Halt_Intentful,
+                procedure_callback: callback
+            };
+            this._procedures[proceduralSignatureForProvider.name] = proceduralSignatureForProvider;
+        }
+        return this;
+    }
+
+    get providerProcedures(): { [p: string]: Procedural_Signature } {
+        return this._procedures;
+    }
+
+    get kindProcedures(): { [p: string]: Procedural_Signature } {
+        return this._kinds[0].procedures;
+    }
+
+    public withKindProcedures(callback: string, value?: { [p: string]: Procedural_Signature }) {
+        if (value === undefined) {
+            const geolocationComputeProceduralSignature: Procedural_Signature = {
+                name: "computeGeolocation",
+                argument: loadYaml("./procedure_geolocation_compute_input.yml"),
+                result: loadYaml("./procedure_geolocation_compute_input.yml"),
+                execution_strategy: Procedural_Execution_Strategy.Halt_Intentful,
+                procedure_callback: callback
+            };
+            if (this._kinds.length >= 1) {
+                this._kinds[0].procedures[geolocationComputeProceduralSignature.name] = geolocationComputeProceduralSignature
+            } else {
+                throw new Error(formatErrorMsg("Kind Procedures", "Kinds"))
+            }
+        }
+        return this;
+    }
+
+    get entityProcedures(): { [p: string]: Procedural_Signature } {
+        return this._kinds[0].procedures;
+    }
+
+    public withEntityProcedures(callback: string, value?: { [p: string]: Procedural_Signature }) {
+        if (value === undefined) {
+            const proceduralSignatureForKind: Procedural_Signature = {
+                name: "moveX",
+                argument: loadYaml("./procedure_move_input.yml"),
+                result: loadYaml("./location_kind_test_data.yml"),
+                execution_strategy: Procedural_Execution_Strategy.Halt_Intentful,
+                procedure_callback: callback
+            };
+            if (this._kinds.length >= 1) {
+                this._kinds[0].procedures[proceduralSignatureForKind.name] = proceduralSignatureForKind
+            } else {
+                throw new Error(formatErrorMsg("Entity Procedures", "Kinds"))
+            }
+        }
+        return this;
+    }
+    get kinds(): Kind[] {
+        return this._kinds;
+    }
+
+    public withKinds(value?: Kind[]) {
+        if (value === undefined) {
+            this._kinds = [getSpecOnlyEntityKind()];
+        } else {
+            this._kinds = value;
+        }
+        return this;
+    }
+
+    get version(): Version {
+        return this._version;
+    }
+
+    public withVersion(value: Version) {
+        this._version = value;
+        return this;
+    }
 }
 
 export function getProviderWithSpecOnlyEnitityKindNoOperations(): Provider {

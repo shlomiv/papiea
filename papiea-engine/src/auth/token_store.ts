bd1a24b52154070f25f5b63da2e5ba021184e511
Shlomi Vaknin <shlomivaknin@gmail.com>
2019-08-07 16:56:52 -0700

    Revert "move to md5 hashing"
    This reverts commit bab045296349d00546ae5015d29b95617ee137e7.

--- papiea-engine/src/auth/token_store.ts
+++ papiea-engine/src/auth/token_store.ts
@@ -1,12 +1,19 @@
-import {Md5} from 'ts-md5/dist/md5'
+function hash_string(str:string):number {
+    let hash = 0, i = 0, len = str.length;
+    while ( i < len ) {
+        hash  = ((hash << 5) - hash + str.charCodeAt(i++)) << 0;
+    }
+    return hash;
+};
 
 export class TokenStore {
     private tokens:any = {};
-        /**
+
+    /**
      * add_token
      */
-    public add_token(token: string): string {
-        const token_hash = <string> Md5.hashAsciiStr(token)
+    public add_token(token: string): number {
+        const token_hash = hash_string(token)
         this.tokens[token_hash] = token
         return token_hash
     }
@@ -14,7 +21,7 @@ export class TokenStore {
     /**
      * get_token
      */
-    public get_token(hash: string) {
+    public get_token(hash: number) {
         return this.tokens[hash]
     }
 }

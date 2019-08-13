0f5e30f869fad9ead976f616a8914183348fe96e
Igor Korotach <igorkorotach@gmail.com>
2019-08-13 12:28:39 +0300

    fixed logout errors not being logged (#274)

--- papiea-engine/src/auth/oauth2.ts
+++ papiea-engine/src/auth/oauth2.ts
@@ -76,7 +76,8 @@ export function createOAuth2Router(logger: Logger, redirect_uri: string, provide
             await sessionKeyAPI.inactivateKey(sessionKey.key)
             await idpToken.revoke('access_token');
         } catch (e) {
-            return res.status(400).json("failed");
+            logger.error('Logout error: ', e.message);
+            return res.status(400).json("Logout failed");
         }
         return res.status(200).json({"logout_uri": `${provider.oauth2.oauth.auth_host}${provider.oauth2.oauth.logout_uri}`});
     }));

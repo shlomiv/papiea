e771aeba2333bc753758568b6329b8dab9ddcf7d
Danylo Lytovchenko <d.lytovchenko@quantumobile.com>
2019-10-23 16:40:39 +0300

    Add policy DB implementation

---
+++ papiea-engine/src/databases/policy_db_mongo.ts
@@ -0,0 +1,40 @@
+import { Policy_DB } from "./policy_db_interface";
+import { Policy } from "papiea-core";
+import { Collection, Db } from "mongodb";
+import { Logger } from "../logger_interface"
+
+export class Policy_DB_Mongo implements Policy_DB {
+    collection: Collection;
+    logger: Logger;
+
+    constructor(logger: Logger, db: Db) {
+        this.collection = db.collection("policy");
+        this.logger = logger;
+    }
+
+    async create_policy(policy: Policy): Promise<void> {
+        await this.collection.insertOne(policy);
+        return;
+    };
+
+    async get_policy(uuid: string): Promise<Policy> {
+        const result: Policy | null = await this.collection.findOne({
+            "uuid": uuid,
+        });
+        if (result === null) {
+            throw new Error("key not found");
+        }
+        return result;
+    };
+
+    async list_policies(fields_map: any): Promise<Policy[]> {
+        const filter: any = Object.assign({}, fields_map);
+        const result = await this.collection.find(filter).toArray();
+        return result;
+    };
+
+    async delete_policy(uuid: string): Promise<void> {
+        await this.collection.deleteOne({uuid: uuid});
+        return;
+    };
+}

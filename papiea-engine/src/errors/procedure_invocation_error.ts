c6890e7c691701d502980febf13eb5bcbcdc2b63
Igor Korotach <igorkorotach@gmail.com>
2019-10-23 21:27:41 +0300

    Fix provider error message propagation (#317)
    * fixed error message propagation from provider & added a test
    
    * narrowed prop check
    
    * reworked sdk error handling
    
    * Bump up version

--- papiea-engine/src/errors/procedure_invocation_error.ts
+++ papiea-engine/src/errors/procedure_invocation_error.ts
@@ -1,11 +1,37 @@
+import { ValidationError } from "./validation_error"
+import { AxiosError } from "axios"
+import { isAxiosError } from "../utils/utils"
+
 export class ProcedureInvocationError extends Error {
     errors: { [key: string]: any }[];
     status: number;
 
-    constructor(errors: { [key: string]: any }[], status: number) {
+    protected constructor(errors: { [key: string]: any }[], status: number) {
         super(JSON.stringify(errors));
         Object.setPrototypeOf(this, ProcedureInvocationError.prototype);
         this.errors = errors;
         this.status = status;
+    }
+
+    static fromError(err: AxiosError): ProcedureInvocationError
+    static fromError(err: ValidationError): ProcedureInvocationError
+    static fromError(err: Error): ProcedureInvocationError {
+        if (isAxiosError(err)) {
+            return new ProcedureInvocationError([{
+                message: err.response!.data.message,
+                errors: err.response!.data.errors,
+                stacktrace: err.response!.data.stacktrace
+            }], err.response!.status)
+        } else if (err instanceof ValidationError) {
+            return new ProcedureInvocationError(err.errors.map(e => {
+                return { message: e }
+            }), 400)
+        } else {
+            return new ProcedureInvocationError([{
+                message: "Unknown error during procedure invocation",
+                errors: {},
+                stacktrace: err.stack
+            }], 500)
+        }
     }
 }

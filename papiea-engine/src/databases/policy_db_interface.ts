00e32d0f0cd3110b08309a42a0f48d10781b4b0b
Danylo Lytovchenko <d.lytovchenko@quantumobile.com>
2019-10-23 16:16:03 +0300

    Add policy DB interface

---
+++ papiea-engine/src/databases/policy_db_interface.ts
@@ -0,0 +1,11 @@
+import { Policy } from "papiea-core";
+
+export interface Policy_DB {
+    create_policy(policy: Policy): Promise<void>;
+
+    get_policy(uuid: string): Promise<Policy>;
+
+    list_policies(fields_map: any): Promise<Policy[]>;
+
+    inactivate_policy(uuid: string): Promise<void>;
+}

8541d857e1a4bbaf1a45f42a5a42ebd9ddf02c64
Igor Korotach <igorkorotach@gmail.com>
2020-03-24 10:39:17 +0200

    fixed get token (#333)

--- papiea-engine/src/auth/authn.ts
+++ papiea-engine/src/auth/authn.ts
@@ -64,7 +64,7 @@ export function asyncHandler(fn: UserAuthRequestHandler): any {
 
 function getToken(req: any): string | null {
     if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-        return req.headers.authorization.split(' ')[1];
+        return req.headers.authorization.split(' ')[1] || '';
     } else if (req.query && req.query.token) {
         return req.query.token;
     } else if (req.cookies && req.cookies.token) {

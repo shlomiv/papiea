b8a0941c3097d01bc3f7056aa4de722dbeaa5875
igorkorotach <igorkorotach@gmail.com>
2020-04-17 11:15:08 +0300

    changed on create|delete semantics, added log messages

--- papiea-engine/src/intentful_core/intentful_strategy_interface.ts
+++ papiea-engine/src/intentful_core/intentful_strategy_interface.ts
@@ -40,8 +40,9 @@ export abstract class IntentfulStrategy {
     }
 
     async create(metadata: Metadata, spec: Spec): Promise<[Metadata, Spec]> {
+        const entity = this.create_entity(metadata, spec)
         await this.dispatch("__create", { metadata, spec })
-        return this.create_entity(metadata, spec)
+        return entity
     }
 
     protected async dispatch(procedure_name: string, entity: Partial<Entity>): Promise<any> {

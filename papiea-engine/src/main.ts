35b17cd6185f225ed25cb4bbe312f4e31747bacb
igorkorotach <igorkorotach@gmail.com>
2020-06-03 14:03:19 +0300

    error message properly displayed

--- papiea-engine/src/main.ts
+++ papiea-engine/src/main.ts
@@ -89,7 +89,7 @@ async function setUpApplication(): Promise<express.Express> {
         const papieaError = PapieaErrorResponseImpl.create(err);
         res.status(papieaError.status)
         res.json(papieaError.toResponse())
-        logger.error(papieaError, err.stack)
+        logger.error(papieaError.toString(), err.stack)
     });
     return app;
 }

f2fe9dbbc13f2a11fd340fb5201c3805afe95e38
igorkorotach <igorkorotach@gmail.com>
2020-07-01 16:55:51 +0300

    added new params to Python SDK

--- papiea-sdk/python/papiea/utils.py
+++ papiea-sdk/python/papiea/utils.py
@@ -1,7 +1,7 @@
 import json
-from typing import Any
+from typing import Any, Optional
 
-from papiea.core import AttributeDict
+from .core import AttributeDict, ErrorSchemas
 
 
 def json_loads_attrs(s: str) -> Any:
@@ -9,3 +9,11 @@ def json_loads_attrs(s: str) -> Any:
         return AttributeDict(obj)
 
     return json.loads(s, object_hook=object_hook)
+
+
+def validate_error_codes(error_schemas: Optional[ErrorSchemas]):
+    if error_schemas:
+        for code in error_schemas:
+            numeric_code = int(code)
+            if not isinstance(numeric_code, int) or not (400 < numeric_code < 599):
+                raise Exception("Error description should feature status code in 4xx or 5xx")

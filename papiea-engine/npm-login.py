b3845e2391c4596ae0420b1100687a3a7dd17d51
igorkorotach <igorkorotach@gmail.com>
2020-08-13 13:31:41 +0300

    circleci integration (+ added NPM_CONFIG_TOKEN to CircleCI panel)

--- papiea-engine/npm-login.py
+++ papiea-engine/npm-login.py
@@ -2,11 +2,13 @@
 import os
 import sys
 
-npm_token = os.environ.get("NPM_TOKEN")
+npm_token = os.environ.get("NPM_CONFIG_TOKEN")
 
 if not npm_token:
     print("No NPM_TOKEN found, exiting")
     sys.exit(1)
 
+# Npm will substitute the token itself
+# Note: env variable name should be prefixed with NPM_CONFIG_
 with open("~/.npmrc", "w") as file:
-    file.write('registry=https://nutanix.jfrog.io/nutanix/api/npm/npm-virtual/\n_auth={}\nalways-auth=true\n'.format(npm_token))
+    file.write('//nutanix.jfrog.io/nutanix/api/npm/npm-virtual/:_authToken=$TOKEN')

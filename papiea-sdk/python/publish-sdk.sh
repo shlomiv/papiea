7c8293b4cffe14d63df6b2d917683e10334fd1bc
igorkorotach <igorkorotach@gmail.com>
2020-09-08 15:58:18 +0300

    chmod and description to the publish script

--- papiea-sdk/python/publish-sdk.sh
+++ papiea-sdk/python/publish-sdk.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+
+# This scripts builds and pushes python distributable
+# to nutanix remote pypi repo
 python3 -m pip install --user --upgrade setuptools wheel
 
+# Version is passed here as a hack
+# since there isn't an easy way to
+# add params to setup.py
 VERSION="$1" python3 setup.py sdist bdist_wheel
 
 python3 -m pip install --user --upgrade twine

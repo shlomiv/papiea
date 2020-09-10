7565552262fc725c3dd5a75c65084694ca4afb5d
igorkorotach <igorkorotach@gmail.com>
2020-09-10 10:33:13 +0300

    try pypi-local instead of remote

--- papiea-sdk/python/publish-sdk.sh
+++ papiea-sdk/python/publish-sdk.sh
@@ -23,4 +23,4 @@ python3 -m pip install --user --upgrade twine
 export TWINE_USERNAME="$ARTIFACTORY_USER"
 export TWINE_PASSWORD="$ARTIFACTORY_PASSWORD"
 
-python3 -m twine upload --repository-url https://nutanix.jfrog.io/nutanix/api/pypi/pypi-remote dist/*
+python3 -m twine upload --repository-url https://nutanix.jfrog.io/nutanix/api/pypi/pypi-local dist/*

7769c3e5cdda4d584a1265313fa3d8aff2c8c3ad
igorkorotach <igorkorotach@gmail.com>
2020-09-10 16:36:40 +0300

    use version w/o + for testpypi

--- papiea-sdk/python/publish-sdk.sh
+++ papiea-sdk/python/publish-sdk.sh
@@ -16,7 +16,7 @@ python3 -m pip install --user --upgrade setuptools wheel
 # Version is passed here as a hack
 # since there isn't an easy way to
 # add params to setup.py
-VERSION="$1" python3 setup.py sdist bdist_wheel
+VERSION="1.0.2020" python3 setup.py sdist bdist_wheel
 
 python3 -m pip install --user --upgrade twine
 

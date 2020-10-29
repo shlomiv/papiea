de7cc16b3a54316a5f4c29e8a4d3b8d9652d5269
igorkorotach <igorkorotach@gmail.com>
2020-10-29 13:19:50 +0200

    add diff path value retrieval

--- papiea-engine/src/intentful_core/differ_impl.ts
+++ papiea-engine/src/intentful_core/differ_impl.ts
@@ -1,6 +1,14 @@
 import { Spec, Status, Kind, Differ, Diff } from "papiea-core"
 import { SFSCompiler } from "./sfs_compiler"
 
+export interface DiffContent {
+    keys: any,
+    key: string,
+    path: Array<number | string>,
+    spec: number[] | string[],
+    status: number[] | string[]
+}
+
 export class BasicDiffer implements Differ {
     // Get the diff iterator from an entity based on the
     public* diffs(kind: Kind, spec: Spec, status: Status): Generator<Diff, any, undefined> {
@@ -30,5 +38,34 @@ export class BasicDiffer implements Differ {
                 }
             }
         ).filter(diff => diff.diff_fields !== null && diff.diff_fields.length > 0)
+    }
+
+    public get_diff_path_value(diff: DiffContent, spec: Spec) {
+        // This depends keys and diff.path values to be in order
+        const keys = Object.keys(diff.keys)
+        const arrayItem = (arr: any[], key: string, value: any) => {
+            const result = arr.filter(val => val[key] === value)
+            if (result[0] !== undefined) {
+                return result
+            } else {
+                throw new Error("Couldn't correctly traverse diff path")
+            }
+        }
+        let obj = spec
+        let key_index = 0
+        for (let item of diff.path) {
+            switch (typeof item) {
+                case "number":
+                    obj = arrayItem(obj, keys[key_index], item)
+                    key_index++
+                    break
+                case "string":
+                    obj = obj[item]
+                    break
+                default:
+                    throw new Error("Unsupported type while traversing diff path")
+            }
+        }
+        return obj
     }
 }
